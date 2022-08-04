89ae71b62ec21c2f2ac831a2d0e52283d2fb64d8
Egor <egorcoders@gmail.com>
2022-08-04 16:56:30 +0500

    added readme, removed comments

--- yatube/about/views.py
+++ yatube/about/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 
-# Create your views here.
 class AboutAuthorView(TemplateView):
     template_name = 'about/author.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Здесь можно произвести какие-то действия для создания контекста.
-        # Для примера в словарь просто передаются две строки
         context['title'] = 'Об авторе проекта'
         context['header'] = ('Привет, я автор')
-        context['text'] = ('Тут я размещу информацию о себе используя свои умения верстать. Картинки, блоки, элементы бустрап. А может быть, просто напишу несколько абзацев текста.')
+        context['text'] = (
+            'Тут я размещу информацию о себе используя свои '
+            'умения верстать. Картинки, блоки, элементы бустрап.'
+            'А может быть, просто напишу несколько абзацев текста.'
+        )
         return context
 
 
@@ -21,8 +21,6 @@ class AboutTechView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Здесь можно произвести какие-то действия для создания контекста.
-        # Для примера в словарь просто передаются две строки
         context['title'] = 'Технологии'
         context['header'] = ('Вот что я умею')
         context['text'] = ('Текст страницы "Технологии"')
